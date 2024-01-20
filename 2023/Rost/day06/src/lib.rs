8a8bfb2a7993d058c7bcc5138bb523695d9a372d
Vaengir <vaengir@gmx.de>
2024-01-20 19:07:00 +0100

    feat: Input parsing done

--- 2023/Rost/day06/src/lib.rs
+++ 2023/Rost/day06/src/lib.rs
@@ -1,8 +1,40 @@
-pub fn process_part1(input: &str) -> usize {
-    todo!();
+#[derive(Debug, Clone, Copy)]
+struct Race {
+    time: usize,
+    distance: usize,
 }
 
-pub fn process_part2(input: &str) -> usize {
+impl Race {
+    fn new(time: usize, distance: usize) -> Race {
+        Race { time, distance }
+    }
+}
+
+pub fn process_part1(input: &str) -> usize {
+    let result: usize = 0;
+    let times: Vec<usize> = input
+        .lines()
+        .next()
+        .unwrap()
+        .split_whitespace()
+        .flat_map(|part| part.parse::<usize>())
+        .collect();
+    let distances: Vec<usize> = input
+        .lines()
+        .nth(1)
+        .unwrap()
+        .split_whitespace()
+        .flat_map(|part| part.parse::<usize>())
+        .collect();
+    let mut races: Vec<Race> = Vec::new();
+    for (i, _) in times.iter().enumerate() {
+        races.push(Race::new(times[i], distances[i]));
+    }
+    dbg!(races);
+    result
+}
+
+pub fn process_part2(_input: &str) -> usize {
     todo!();
 }
 
@@ -10,9 +42,13 @@ pub fn process_part2(input: &str) -> usize {
 mod tests {
     use super::*;
 
+    const INPUT1: &str = "\
+Time:      7  15   30
+Distance:  9  40  200";
+
     #[test]
     fn part1_works() {
-        todo!();
+        assert_eq!(process_part1(INPUT1), 288);
     }
 
     #[test]
