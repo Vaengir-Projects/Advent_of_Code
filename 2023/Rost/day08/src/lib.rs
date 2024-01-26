5dbf8b164400f344cc5321a9bf098d1b10bec8e3
Vaengir <vaengir@gmx.de>
2024-01-26 15:00:02 +0100

    feat: Part 2
    Works but performance is horrible.

--- 2023/Rost/day08/src/lib.rs
+++ 2023/Rost/day08/src/lib.rs
@@ -61,8 +61,44 @@ pub fn process_part1(input: &str) -> usize {
     result
 }
 
-pub fn process_part2(_input: &str) -> usize {
-    todo!();
+pub fn process_part2(input: &str) -> usize {
+    let mut result: usize = 0;
+    let navigation: Vec<char> = input.split("\n\n").next().unwrap().chars().collect();
+    let mut nodes: Vec<Node> = Vec::new();
+    for line in input.split("\n\n").nth(1).unwrap().lines() {
+        nodes.push(Node::build(line));
+    }
+    let mut starters: Vec<Node> = nodes
+        .iter()
+        .cloned()
+        .filter(|nod| nod.key.ends_with('A'))
+        .collect();
+    let mut navigation_cycle = navigation.iter().cycle();
+    loop {
+        if starters.iter().all(|nod| nod.key.ends_with('Z')) {
+            break;
+        }
+        let nav = *navigation_cycle.next().unwrap();
+        let mut temp_starter: Vec<Node> = vec![Node::new(); starters.len()];
+        for (i, node) in starters.iter().enumerate() {
+            if nav == 'R' {
+                temp_starter[i] = nodes
+                    .iter()
+                    .find(|nod| nod.key == node.elements[1])
+                    .unwrap()
+                    .clone()
+            } else {
+                temp_starter[i] = nodes
+                    .iter()
+                    .find(|nod| nod.key == node.elements[0])
+                    .unwrap()
+                    .clone()
+            }
+        }
+        starters = temp_starter;
+        result += 1;
+    }
+    result
 }
 
 #[cfg(test)]
@@ -87,6 +123,18 @@ AAA = (BBB, BBB)
 BBB = (AAA, ZZZ)
 ZZZ = (ZZZ, ZZZ)";
 
+    const INPUT2: &str = "\
+LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)";
+
     #[test]
     fn part1_works() {
         assert_eq!(process_part1(INPUT0), 2);
@@ -95,6 +143,6 @@ ZZZ = (ZZZ, ZZZ)";
 
     #[test]
     fn part2_works() {
-        todo!();
+        assert_eq!(process_part2(INPUT2), 6);
     }
 }
