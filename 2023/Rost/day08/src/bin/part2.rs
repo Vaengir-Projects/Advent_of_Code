9b1413dc285e780dfab0d7ebc24148166b76e8d5
Vaengir <vaengir@gmx.de>
2024-01-30 21:35:01 +0100

    feat: Two better versions of part2
    One is just a cleaner version than mine.
    Second is way faster and uses least common multiple.

--- 2023/Rost/day08/src/bin/part2.rs
+++ 2023/Rost/day08/src/bin/part2.rs
@@ -1,7 +1,11 @@
-use day08::process_part2;
+// use day08::process_part2;
+// use day08::process_part2_better;
+use day08::process_part2_try2;
 use std::fs;
 
 fn main() {
     let file = fs::read_to_string("input.txt").unwrap();
-    println!("{}", process_part2(&file))
+    // println!("{}", process_part2(&file));
+    // println!("{}", process_part2_better(&file));
+    println!("{}", process_part2_try2(&file));
 }
--- 2023/Rost/day08/src/lib.rs
+++ 2023/Rost/day08/src/lib.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 #[derive(Debug, Clone)]
 pub struct Node {
     key: String,
@@ -101,6 +103,142 @@ pub fn process_part2(input: &str) -> usize {
     result
 }
 
+#[derive(Debug)]
+pub struct Map {
+    instructions: Vec<Direction>,
+    nodes: HashMap<String, NodeBetter>,
+}
+
+impl Map {
+    fn parse(input: &str) -> Self {
+        let mut split = input.split("\n\n");
+        let instructions = split.next().unwrap();
+        let nodes = split.next().unwrap();
+        let instructions = instructions
+            .chars()
+            .map(|c| match c {
+                'L' => Direction::Left,
+                'R' => Direction::Right,
+                _ => panic!("Invalid direction"),
+            })
+            .collect::<Vec<_>>();
+        let nodes = nodes
+            .lines()
+            .map(NodeBetter::parse)
+            .map(|n| (n.name.clone(), n))
+            .collect::<HashMap<String, NodeBetter>>();
+        Self {
+            instructions,
+            nodes,
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum Direction {
+    Left,
+    Right,
+}
+
+#[derive(Debug)]
+pub struct NodeBetter {
+    name: String,
+    left: String,
+    right: String,
+}
+
+impl NodeBetter {
+    fn parse(input: &str) -> Self {
+        let mut name_and_connections = input.split(" = ");
+        let name = name_and_connections.next().unwrap();
+        let connections = name_and_connections.next().unwrap();
+        let connections = connections
+            .strip_prefix("(")
+            .unwrap()
+            .strip_suffix(")")
+            .unwrap();
+        let mut connections = connections.split(", ");
+        let left = connections.next().unwrap();
+        let right = connections.next().unwrap();
+        Self {
+            name: name.to_string(),
+            left: left.to_string(),
+            right: right.to_string(),
+        }
+    }
+}
+
+pub fn process_part2_better(input: &str) -> usize {
+    let map = Map::parse(input);
+    let starting_positions = map
+        .nodes
+        .iter()
+        .filter(|(_, n)| n.name.ends_with('A'))
+        .map(|(_, n)| n.name.clone())
+        .collect::<Vec<_>>();
+    let mut count = 0;
+    let mut current_node_names = starting_positions;
+    let mut intruction_iter = map.instructions.iter().cycle();
+    while !current_node_names.iter().all(|n| n.ends_with('Z')) {
+        let current_instruction = intruction_iter.next().unwrap();
+        for current_node_name in current_node_names.iter_mut() {
+            let current_node = map.nodes.get(current_node_name).unwrap();
+            let next_node_name = match current_instruction {
+                Direction::Left => current_node.left.clone(),
+                Direction::Right => current_node.right.clone(),
+            };
+            *current_node_name = next_node_name;
+        }
+        count += 1;
+    }
+    count
+}
+
+fn least_common_multiple(nums: &[usize]) -> usize {
+    let mut result = 1;
+    for &num in nums {
+        result = num * result / gcd(num, result);
+    }
+    result
+}
+
+fn gcd(a: usize, b: usize) -> usize {
+    if b == 0 {
+        return a;
+    }
+    gcd(b, a % b)
+}
+
+pub fn process_part2_try2(input: &str) -> usize {
+    let map = Map::parse(input);
+    let starting_positions = map
+        .nodes
+        .iter()
+        .filter(|(_, n)| n.name.ends_with('A'))
+        .map(|(_, n)| n.name.clone())
+        .collect::<Vec<_>>();
+    let mut to_end_counts = HashMap::<String, usize>::new();
+    for start in starting_positions.iter() {
+        let mut current_node_name = start.clone();
+        let mut count = 0;
+        let mut instruction_iter = map.instructions.iter().cycle();
+        while !current_node_name.ends_with('Z') {
+            let current_instruction = instruction_iter.next().unwrap();
+            let current_node = map.nodes.get(&current_node_name).unwrap();
+            let next_node_name = match current_instruction {
+                Direction::Left => current_node.left.clone(),
+                Direction::Right => current_node.right.clone(),
+            };
+            current_node_name = next_node_name;
+            count += 1;
+        }
+        to_end_counts.insert(start.clone(), count);
+    }
+    let counts = to_end_counts.values().cloned().collect::<Vec<_>>();
+    let lcm = least_common_multiple(&counts);
+    lcm
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -144,5 +282,15 @@ XXX = (XXX, XXX)";
     #[test]
     fn part2_works() {
         assert_eq!(process_part2(INPUT2), 6);
+    }
+
+    #[test]
+    fn part2better_works() {
+        assert_eq!(process_part2_better(INPUT2), 6);
+    }
+
+    #[test]
+    fn part2try2_works() {
+        assert_eq!(process_part2_try2(INPUT2), 6);
     }
 }
