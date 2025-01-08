c219cbd900fa6e1afe50d0ed2564088bf72df81a
Vaengir <vaengir@gmx.de>
2025-01-08 21:49:33 +0100

    feat: Day 2 2024 Part2

--- 2024/Rost/day02/src/lib.rs
+++ 2024/Rost/day02/src/lib.rs
@@ -37,6 +37,102 @@ pub fn process_part1(input: &str) -> usize {
     sum
 }
 
+pub fn process_part2(input: &str) -> usize {
+    let mut sum: usize = 0;
+    let lines = input.lines();
+    for line in lines {
+        let levels: Vec<usize> = line
+            .split_whitespace()
+            .map(|x| x.parse::<usize>().unwrap())
+            .collect();
+        let up_or_down: Option<bool> = None;
+        if check_levels(&levels, up_or_down, false) {
+            sum += 1;
+        }
+    }
+
+    sum
+}
+
+fn check_levels(levels: &[usize], mut up_or_down: Option<bool>, bad: bool) -> bool {
+    for (i, _) in levels.iter().enumerate().take(levels.len() - 1) {
+        if (levels[i].abs_diff(levels[i + 1]) > 3) || (levels[i] == levels[i + 1]) {
+            if bad {
+                return false;
+            }
+            let mut temp_vec1;
+            let mut temp_vec2;
+            if i == 1 {
+                up_or_down = None;
+                temp_vec1 = levels.to_vec();
+                let _ = temp_vec1.remove(i - 1);
+                temp_vec2 = levels.to_vec();
+                let _ = temp_vec2.remove(i);
+            } else {
+                temp_vec1 = levels.to_vec();
+                let _ = temp_vec1.remove(i);
+                temp_vec2 = levels.to_vec();
+                let _ = temp_vec2.remove(i + 1);
+            }
+            return check_levels(&temp_vec1, up_or_down, true)
+                || check_levels(&temp_vec2, up_or_down, true);
+        }
+        match up_or_down {
+            Some(true) => {
+                if levels[i] <= levels[i + 1] {
+                    if bad {
+                        return false;
+                    }
+                    let mut temp_vec1;
+                    let mut temp_vec2;
+                    if i == 1 {
+                        up_or_down = None;
+                        temp_vec1 = levels.to_vec();
+                        let _ = temp_vec1.remove(i - 1);
+                        temp_vec2 = levels.to_vec();
+                        let _ = temp_vec2.remove(i);
+                    } else {
+                        temp_vec1 = levels.to_vec();
+                        let _ = temp_vec1.remove(i);
+                        temp_vec2 = levels.to_vec();
+                        let _ = temp_vec2.remove(i + 1);
+                    }
+                    return check_levels(&temp_vec1, up_or_down, true)
+                        || check_levels(&temp_vec2, up_or_down, true);
+                }
+            }
+            Some(false) => {
+                if levels[i] >= levels[i + 1] {
+                    if bad {
+                        return false;
+                    }
+                    let mut temp_vec1;
+                    let mut temp_vec2;
+                    if i == 1 {
+                        up_or_down = None;
+                        temp_vec1 = levels.to_vec();
+                        let _ = temp_vec1.remove(i - 1);
+                        temp_vec2 = levels.to_vec();
+                        let _ = temp_vec2.remove(i);
+                    } else {
+                        temp_vec1 = levels.to_vec();
+                        let _ = temp_vec1.remove(i);
+                        temp_vec2 = levels.to_vec();
+                        let _ = temp_vec2.remove(i + 1);
+                    }
+                    return check_levels(&temp_vec1, up_or_down, true)
+                        || check_levels(&temp_vec2, up_or_down, true);
+                }
+            }
+            None => {
+                up_or_down = Some(levels[i] > levels[i + 1]);
+            }
+        }
+    }
+
+    true
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -49,8 +145,41 @@ mod tests {
 8 6 4 4 1
 1 3 6 7 9";
 
+    const INPUT24: &[usize] = &[24, 21, 22, 24, 25, 26, 27];
+
+    const INPUT131: &[usize] = &[35, 34, 31, 34, 30];
+
+    const INPUT148: &[usize] = &[80, 82, 81, 80, 77, 76, 73, 72];
+
+    const INPUT287: &[usize] = &[5, 4, 8, 9, 12, 13];
+
     #[test]
     fn part1_works() {
         assert_eq!(process_part1(INPUT1), 2);
+    }
+
+    #[test]
+    fn part2_works() {
+        assert_eq!(process_part2(INPUT1), 4);
+    }
+
+    #[test]
+    fn line_24_works() {
+        assert!(check_levels(INPUT24, None, false));
+    }
+
+    #[test]
+    fn line_131_works() {
+        assert!(check_levels(INPUT131, None, false));
+    }
+
+    #[test]
+    fn line_148_works() {
+        assert!(check_levels(INPUT148, None, false));
+    }
+
+    #[test]
+    fn line_287_works() {
+        assert!(check_levels(INPUT287, None, false));
     }
 }
