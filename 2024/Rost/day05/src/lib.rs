92778447511356480dfa6962f2b618f55731faac
Vaengir <vaengir@gmx.de>
2025-01-16 18:39:26 +0100

    feat: Day 05 2024 Part2

--- 2024/Rost/day05/src/lib.rs
+++ 2024/Rost/day05/src/lib.rs
@@ -1,3 +1,6 @@
+use std::cmp::Ordering;
+use std::collections::HashMap;
+
 pub fn process_part1(input: &str) -> usize {
     let mut sum: usize = 0;
     let mut rules: Vec<(usize, usize)> = Vec::new();
@@ -51,6 +54,66 @@ fn check_valid1(sec: &[usize], rules: Vec<(usize, usize)>) -> bool {
     valid
 }
 
+pub fn process_part2(input: &str) -> usize {
+    let mut sum: usize = 0;
+    let mut rules: HashMap<usize, Vec<usize>> = HashMap::new();
+    let mut sections: Vec<Vec<usize>> = Vec::new();
+
+    for line in input.lines() {
+        let ordering: Vec<&str> = line.split('|').collect();
+        if ordering.len() < 2 && !ordering.is_empty() && !ordering[0].is_empty() {
+            sections.push(
+                ordering[0]
+                    .split(',')
+                    .map(|n| n.parse::<usize>().unwrap())
+                    .collect(),
+            );
+        } else if ordering.len() == 2 {
+            let x: Vec<usize> = ordering
+                .iter()
+                .map(|u| u.parse::<usize>().unwrap())
+                .collect();
+            if let std::collections::hash_map::Entry::Vacant(e) = rules.entry(x[0]) {
+                e.insert(vec![x[1]]);
+            } else {
+                rules.entry(x[0]).and_modify(|a| a.push(x[1]));
+            }
+        }
+    }
+    for sec in sections {
+        sum += check_valid2(sec.clone(), rules.clone());
+    }
+
+    sum
+}
+
+fn check_valid2(sec: Vec<usize>, rules: HashMap<usize, Vec<usize>>) -> usize {
+    let mut valid: bool = false;
+    let mut section = sec.clone();
+    for (i, s) in sec.iter().enumerate() {
+        if let Some(relevant_rules) = rules.get(s) {
+            for rr in relevant_rules {
+                if sec[..i].contains(rr) {
+                    valid = true;
+                }
+            }
+        }
+    }
+
+    if valid {
+        section.sort_by(|a, b| {
+            if rules.get(a).is_some_and(|s| s.contains(b)) {
+                Ordering::Less
+            } else {
+                Ordering::Greater
+            }
+        });
+
+        return section[section.len() / 2];
+    }
+    0
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -88,5 +151,10 @@ mod tests {
     #[test]
     fn part1_works() {
         assert_eq!(process_part1(INPUT1), 143);
+    }
+
+    #[test]
+    fn part2_works() {
+        assert_eq!(process_part2(INPUT1), 123);
     }
 }
